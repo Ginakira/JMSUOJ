fff6ecfc12fe49a938d3fa7347e707058a15ebbb
Ginakira <ginakira@foxmail.com>
2020-03-09 16:18:13 +0800

    [Style]注册页样式重写&提示信息翻译为中文

--- lang/cn.php
+++ lang/cn.php
@@ -91,7 +91,7 @@ $MSG_SOVLED = "解决";
 $MSG_RATIO = "比率";
 
 //registerpage.php
-$MSG_USER_ID = "用户名（学号）";
+$MSG_USER_ID = "用户ID";
 $MSG_PASSWORD = "密码";
 $MSG_REPEAT_PASSWORD = "重复密码";
 $MSG_SCHOOL = "学校";
