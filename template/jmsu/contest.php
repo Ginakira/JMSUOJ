a6356aaa23b4500c00d59566e81d6ce8f6d1a6f7
Ginakira <ginakira@foxmail.com>
2020-03-29 12:55:40 +0800

    [Style] 部分页面添加图标

--- template/jmsu/contest.php
+++ template/jmsu/contest.php
@@ -32,16 +32,19 @@
             </div>
             <div class="col text-center">
                 <div class="alert alert-info">
-                    开始时间:
-                    <font class='text-info'><?php echo $view_start_time ?></font>&nbsp;
-                    结束时间: <font class='text-info'><?php echo $view_end_time ?></font><br>
+                    <i class="fas fa-calendar-check"></i> 开始时间:
+                    <font class='text-info'><?php echo $view_start_time ?></font><br>
+                    <i class="fas fa-calendar-times"></i> 结束时间: <font
+                            class='text-info'><?php echo $view_end_time ?></font><br>
                     <?php if (isset($OJ_RANK_LOCK_PERCENT) && $OJ_RANK_LOCK_PERCENT != 0) { ?>
-                        榜单锁定时间: <font color=#993399><?php echo date("Y-m-d H:i:s", $view_lock_time) ?></font>
+                        <i class="fas fa-clock"></i> 榜单锁定时间: <font
+                                color=#993399><?php echo date("Y-m-d H:i:s", $view_lock_time) ?></font>
                         <br/>
                     <?php } ?>
-                    当前服务器时间: <font class='text-info'><span id=nowdate> <?php echo date("Y-m-d H:i:s") ?></span></font>
+                    <i class="fas fa-clock"></i> 当前服务器时间: <font class='text-info'><span
+                                id=nowdate> <?php echo date("Y-m-d H:i:s") ?></span></font>
                     <br/>
-                    状态：<?php
+                    <i class="fas fa-info-circle"></i> 状态：<?php
                     if ($now > $end_time) {
                         echo " <span class='text-danger'>已结束</span>";
                     } else if ($now < $start_time) {
@@ -51,7 +54,7 @@
                     }
 
                     ?>&nbsp;&nbsp;
-                    类别：
+                    <i class="fas fa-bell"></i> 类别：
                     <?php
                     if ($view_private == '0') {
                         echo " <span class='text-primary'>公开</font>";
