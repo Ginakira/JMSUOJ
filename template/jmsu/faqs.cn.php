bb3a951132c4f1cd5a9f81d375ca4e603e3e70fe
Ginakira <ginakira@foxmail.com>
2020-06-05 00:22:25 +0800

    [Update] 更新时间相关信息修正，去掉FAQ页的更新信息

--- template/jmsu/faqs.cn.php
+++ template/jmsu/faqs.cn.php
@@ -132,9 +132,8 @@
                     <table width=100% border=0>
                         <tr>
                             <td align=right width=65%>
-                                <a href="https://github.com/Ginakira/JMSUOJ"><font color=red><?php echo $OJ_NAME ?>
-                                        Update
-                                        Time:20200325</font></a></td>
+                                <a href="https://github.com/Ginakira/JMSUOJ"><font
+                                            color=red><?php echo $OJ_NAME ?></font></a></td>
                         </tr>
                     </table>
                 </center>
