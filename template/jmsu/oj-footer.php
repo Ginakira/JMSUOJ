3c41358509455e2209f357f6663a0fd56abf2ac9
Ginakira <ginakira@foxmail.com>
2020-06-05 12:17:28 +0800

    [Update] 版本修正

--- template/jmsu/oj-footer.php
+++ template/jmsu/oj-footer.php
@@ -5,7 +5,7 @@
                 <img src='image/logo-jmsu.png' class="img-fluid" style="transform: scale(0.6);" alt='JMSU-ACM logo'>
             </div>
             <div class="col-lg-6 small">
-                <span class="text-muted">JMSU Online Judge V2.0.2 build200605</span><br>
+                <span class="text-muted">JMSU Online Judge V2.0.3 build200605</span><br>
                 <span class="text-muted">开发/维护：<a href='https://github.com/Ginakira/JMSUOJ'>AutoWA Team</a></span>
                 <span class="text-muted">基于开源项目HUSTOJ</span><br>
                 <span class="text-muted">Copyright © 2019-<?php echo date("Y") ?> AutoWA Team All Rights Reserved.</span><br>
